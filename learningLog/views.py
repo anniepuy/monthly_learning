2b1d5059d0b9571aa22bc062dddbf440fc66a96f
Ann M Hagan <39930638+anniepuy@users.noreply.github.com>
2022-09-26 23:41:04 -0500

    got urls working, added dynamic urls

--- learningLog/views.py
+++ learningLog/views.py
@@ -1,7 +1,33 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
-def index(request):
-    return HttpResponse("This works")
+def monthly_learning(request, month):
+    if month == "janauary":
+        learning_text = "Learned Bootstrap with NuCamp Coding Bootcamp"
+    elif month == "february":
+        learning_text = "Learned React with NuCamp Coding Bootcamp"
+    elif month == "march":
+        learning_text = "Continued to learn React with NuCamp Coding Bootcamp"
+    elif month == "april":
+        learning_text = "Learned React Native with NuCamp Coding Bootcamp"
+    elif month == "may":
+        learning_text = "Continued to learn React Native with NuCamp Coding Bootcamp"
+    elif month == "june":
+        learning_text = "Studied and passed Comptia Security +"
+    elif month == "july":
+        learning_text = "Learned Python with NuCamp Coding Bootcamp"
+    elif month == "august":
+        learning_text = "Learned Sql and Python with NuCamp Coding Bootcamp"
+    elif month == "september":
+        learning_text = "Learned DevOps and CI/CD with NuCamp Coding Bootcamp"
+    elif month == "october":
+        learning_text = "Projected to complete the Linux Foundation's DevOps Bootcamp"
+    elif month == "november":
+        learning_text = "Projected to complete the Linux Foundations Cloud Engineering Bootcamp"
+    elif month == "december":
+        learning_text = "Projected to head back to React and Node.js to strengthen full stack skills."
+    else:
+        return HttpResponseNotFound("Is there another month?  Amazing, I must have slept through it. Sorry, not found.")
+    return HttpResponse(learning_text)
--- monthly_learning/urls.py
+++ monthly_learning/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('learningLog/', include('learningLog.urls'))
+    path('', include('learningLog.urls'))
 ]
